14caae458384cbba00b64027a332e9eb32123f86
iceu-bb <adamlaskowski737@gmail.com>
2019-12-13 16:55:56 +0100

    Add icons and some styling

--- components/PicsList.tsx
+++ components/PicsList.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import {
   StyleSheet,
   View,
   Text,
   TouchableOpacity,
   ScrollView,
-  Image
+  Image,
+  RefreshControl
 } from 'react-native';
 import { NavigationStackProp } from 'react-navigation-stack';
 import { getFunnyPictures } from '../services/xkcdApi';
+import { LinearGradient } from 'expo-linear-gradient';
 
 interface Props {
   navigation: NavigationStackProp;
@@ -25,8 +27,22 @@ interface Picture {
   day: string;
 }
 
+function wait(timeout) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
+
 export const PicsList: React.FC<Props> = ({ navigation }) => {
   const [pics, setPics] = useState<Picture[]>([]);
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+
+    wait(1500).then(() => setRefreshing(false));
+    fetchPictures();
+  }, [refreshing]);
 
   const fetchPictures = async () => {
     const funnyPics = await getFunnyPictures();
@@ -37,49 +53,86 @@ export const PicsList: React.FC<Props> = ({ navigation }) => {
     fetchPictures();
   }, []);
 
+  const getDay = useCallback((day: number, month: number, year: number) => {
+    if (day === undefined || month === undefined || day === undefined) return;
+
+    const today = Date.now();
+    const pictureReleaseDate = new Date(year, month !== 0 ? month - 1 : 0, day);
+    const diffrenceMiliseconds = today - pictureReleaseDate.getTime();
+    return Math.ceil(diffrenceMiliseconds / (1000 * 60 * 60 * 24));
+  }, []);
+
+  if (pics.length < 2)
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+
   return (
-    <ScrollView style={styles.scrollView}>
-      {pics.map(pic => (
-        <TouchableOpacity
-          key={pic.num}
-          onPress={() => {
-            navigation.navigate('PicDetail', {
-              title: pic.safe_title,
-              img: pic.img
-            });
-          }}
-        >
-          <View style={styles.itemContainer}>
-            <Text style={styles.title}>{pic.safe_title}</Text>
-            <Image
-              style={styles.picture}
-              source={{ uri: `${pic.img}` }}
-              resizeMode='contain'
-            />
-          </View>
-        </TouchableOpacity>
-      ))}
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <LinearGradient colors={['#129C8D', '#1BB08A', '#23C186']}>
+        {pics.map(pic => {
+          const { num, img, safe_title, day, month, year } = pic;
+          return (
+            <TouchableOpacity
+              key={num}
+              onPress={() => {
+                navigation.navigate('PicDetail', {
+                  title: safe_title,
+                  img
+                });
+              }}
+            >
+              <View style={styles.itemContainer}>
+                <View style={styles.textContainer}>
+                  <Text style={styles.title}>{safe_title}</Text>
+                  <Text style={styles.subTitle}>
+                    {getDay(Number(day), Number(month), Number(year))} days ago
+                  </Text>
+                </View>
+
+                <Image
+                  style={styles.picture}
+                  source={{ uri: `${img}` }}
+                  resizeMode='cover'
+                />
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </LinearGradient>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: 'beige'
-  },
   itemContainer: {
-    margin: 15,
-    padding: 15,
-    backgroundColor: '#ccc',
+    margin: 20,
+    padding: 20,
+    backgroundColor: 'rgba(0, 0, 0, .15)',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  textContainer: {
+    width: '60%'
   },
   title: {
-    fontSize: 20
+    fontSize: 20,
+    marginBottom: 5,
+    color: '#fff'
+  },
+  subTitle: {
+    fontWeight: '300'
   },
   picture: {
-    width: 100,
+    width: '40%',
     height: 100
   }
 });
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Platform, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -6,43 +8,103 @@ import { About } from '../components/About';
 import { PicsList } from '../components/PicsList';
 import { PicDetailed } from '../components/PicDetailed';
 
-const PicsListStack = createStackNavigator({
-  PicsList: {
-    screen: PicsList,
-    navigationOptions: {
-      headerTitle: 'Latest XKCD memes'
+const PicsListStack = createStackNavigator(
+  {
+    PicsList: {
+      screen: PicsList,
+      navigationOptions: {
+        headerTitle: 'XKCD memes',
+        headerStyle: {
+          backgroundColor: '#C5E2EF'
+        }
+      }
+    },
+    PicDetail: {
+      screen: PicDetailed,
+      navigationOptions: ({ navigation }) => ({
+        headerTitle: navigation.getParam('title'),
+        headerStyle: {
+          backgroundColor: '#24C386'
+        }
+      })
     }
   },
-  PicDetail: {
-    screen: PicDetailed,
-    navigationOptions: ({ navigation }) => ({
-      headerTitle: navigation.getParam('title')
-    })
+  {
+    headerMode: 'float',
+    headerTransitionPreset: 'uikit',
+    headerLayoutPreset: 'center'
   }
-});
+);
 
 const AboutSectionStack = createStackNavigator({
   AboutSection: {
     screen: About,
     navigationOptions: {
-      headerTitle: 'About'
+      headerTitle: 'About',
+      headerStyle: {
+        backgroundColor: '#A8D9F7'
+      }
     }
   }
 });
 
-const Tabs = createBottomTabNavigator({
-  PicsList: {
-    screen: PicsListStack,
-    navigationOptions: {
-      tabBarLabel: 'Memes'
+const Tabs = createBottomTabNavigator(
+  {
+    PicsList: {
+      screen: PicsListStack,
+      navigationOptions: {
+        tabBarLabel: 'Memes'
+      }
+    },
+    AboutSection: {
+      screen: AboutSectionStack,
+      navigationOptions: {
+        tabBarLabel: 'About'
+      }
     }
   },
-  AboutSection: {
-    screen: AboutSectionStack,
-    navigationOptions: {
-      tabBarLabel: 'About'
-    }
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarOptions: {
+        activeTintColor: '#e91e63',
+        labelStyle: {
+          fontSize: 10
+        },
+        style: {
+          backgroundColor: '#eee'
+        }
+      },
+
+      tabBarIcon: ({ tintColor, focused }) => {
+        let image;
+        const { routeName } = navigation.state;
+
+        if (routeName === 'PicsList') {
+          image = Platform.select({
+            ios: require('../assets/icons/ios-list.png'),
+            android: require('../assets/icons/md-list.png')
+          });
+        } else {
+          image = Platform.select({
+            ios: focused
+              ? require('../assets/icons/ios-star.png')
+              : require('../assets/icons/ios-star-outline.png'),
+            android: focused
+              ? require('../assets/icons/md-star.png')
+              : require('../assets/icons/md-star-outline.png')
+          });
+        }
+
+        return (
+          <Image
+            source={image}
+            resizeMode='contain'
+            style={{ width: 25, tintColor }}
+          />
+        );
+      }
+    })
   }
-});
+);
 
 export default createAppContainer(Tabs);
